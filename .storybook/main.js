64815bb24d673fbbab7c0a0ab1ef1f2180bbdd9b
Bryan Holbrook <bryandholbrook@gmail.com>
2021-05-28 15:34:26 -0700

    redux updates. redesign data passing to hooks

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,4 @@
 module.exports = {
-- stories: [
--   '../src/**/*.stories.mdx',
--   '../src/**/*.stories.@(js|jsx|ts|tsx)'
-- ],
 stories: ['../src/components/**/*.stories.js'],
   addons: [
     '@storybook/addon-links',
